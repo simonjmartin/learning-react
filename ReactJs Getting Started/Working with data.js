60643094d2db9f8dd5b7f75b892293d794a6ea0b
Simon Martin (simon.martin@canterbury.ac.uk) <simon.martin@canterbury.ac.uk>
2017-04-19 09:06:03 +0100

    Working with data tutorial
    Pluralsight beginner React: https://app.pluralsight.com/library/courses/react-js-getting-started/table-of-contents. Uses https://jscomplete.com/repl to explore React.
    Simple UI that takes a username from github, fetches the data and displays a card for the user.
    Components are: Card, CardList, Form and App and there's passing functions from the App down via props to Form to allow data to be passed back.

---
+++ ReactJs Getting Started/Working with data.js
@@ -0,0 +1,70 @@
+const Card = (props) => {
+	return (
+  	<div style={{margin: '1em'}}>
+    	<img width="75" src={props.avatar_url} />
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+      	<span style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</span>
+        <div>{props.company}</div>
+      </div>
+    </div>
+  );
+};
+
+const CardList = (props) => {
+	return (
+  	<div>
+    	{props.cards.map(card=> <Card key={card.id} {...card} />)}
+    </div>
+  )
+}
+
+class Form extends React.Component {
+	state = { userName: '' }
+  
+	handleSubmit = (event) => {
+  	event.preventDefault();
+    console.log('Event: form submit', this.state.userName);
+    
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    	.then(resp=> {
+      	this.props.onSubmit(resp.data);
+        this.setState({userName: ''});
+      })
+  };
+
+	render() {
+  	return (
+    	<form onSubmit={this.handleSubmit}>
+      	<input type="text" 
+        			 placeholder="Github username" 
+               value={this.state.userName}
+               onChange={(event) => this.setState({userName: event.target.value})}
+               required />
+        <button type="submit">Add card</button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+	state= {
+      cards: []
+  };
+  
+  addNewCard = (cardInfo) => {
+  	this.setState(prevState => ({
+    	cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+  
+	render() {
+  	return (
+    	<div>
+    		<Form onSubmit={this.addNewCard} />
+      	<CardList cards={this.state.cards} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, mountNode);
