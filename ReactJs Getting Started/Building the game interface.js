924a97060534d2316dad7977d8bab487ce03f50f
Simon Martin (simon.martin@canterbury.ac.uk) <simon.martin@canterbury.ac.uk>
2017-04-21 14:52:41 +0100

    Selecting a number stubbed updates state

--- ReactJs Getting Started/Building the game interface.js
+++ ReactJs Getting Started/Building the game interface.js
@@ -21,18 +21,25 @@ const Button = (props) => {
 const Answer = (props) => {
 	return (
   	<div className="col-5">
-    	<span>5</span>
-      <span>6</span>
+    	{props.selectedNumbers.map((number, i) =>
+      	<span key={i}>{number}</span>
+      )}
     </div>
   )
 }
 
 const Numbers = (props) => {
+	const numberClassName = (number) => {
+  	if (props.selectedNumbers.indexOf(number) >=0){
+    	return 'selected';
+    }
+  }
+  
 	return (
   	<div className="card text-center">
     	<div>
       	{Numbers.list.map((number, i) =>
-        	<span key={i}>{number}</span>
+        	<span key={i} className={numberClassName(number)}>{number}</span>
         )}
       </div>
     </div>
@@ -41,6 +48,10 @@ const Numbers = (props) => {
 Numbers.list = _.range(1, 10);
 
 class Game extends React.Component {
+  state = {
+		selectedNumbers: [],
+  };
+  
 	render() {
   	return (
     	<div>
@@ -49,10 +60,10 @@ class Game extends React.Component {
         <div className="row">
         	<Stars />
         	<Button />
-        	<Answer />
+        	<Answer selectedNumbers={this.state.selectedNumbers} />
         </div>
         <br />
-        <Numbers />
+        <Numbers selectedNumbers={this.state.selectedNumbers} />
       </div>
     )
   }
