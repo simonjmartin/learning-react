1ae6533dc427091771b7b95362e761baa56bc38f
Simon Martin (simon.martin@canterbury.ac.uk) <simon.martin@canterbury.ac.uk>
2017-04-24 16:01:16 +0100

    Basic NPM structure with Gulp build tasks to 'publish'

---
+++ ReactEnvTemplate/gulpfile.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); // local dev server
+var open = require('gulp-open'); // open URL in browser
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        dist: './dist'
+    }
+}
+
+// start local dev server
+gulp.task('connect', function () {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+});
+
+gulp.task('open', ['connect'], function () {
+    gulp.src('dist/index.html')
+        .pipe(open({
+            url: config.devBaseUrl + ':' + config.port + '/'
+        }));
+});
+
+gulp.task('html', function () {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'open']);
--- readme.md
+++ readme.md
@@ -3,4 +3,5 @@
 This is simply a repo for the bits of code that have come from following tutorials etc.  
 Hanging on to them for future reference, until I'm more comfortable
 
-ReactJS Getting Started: https://app.pluralsight.com/library/courses/react-js-getting-started/table-of-contents
+* ReactJS Getting Started: https://app.pluralsight.com/library/courses/react-js-getting-started/table-of-contents
+* ReactEnvTemplate: https://app.pluralsight.com/library/courses/react-flux-building-applications/table-of-contents
