30492818e9d59e99374fc5ffd094531e98687503
Simon Martin (simon.martin@canterbury.ac.uk) <simon.martin@canterbury.ac.uk>
2017-04-25 15:04:50 +0100

    Js watched and bundled

--- ReactEnvTemplate/gulpfile.js
+++ ReactEnvTemplate/gulpfile.js
@@ -12,7 +12,9 @@ var config = {
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        dist: './dist',
+        js: './src/**/.js',
+        mainJs: './src/main.js'
     }
 }
 
@@ -39,8 +41,19 @@ gulp.task('html', function () {
         .pipe(connect.reload());
 });
 
-gulp.task('watch', function () {
-    gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function () {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('watch', function () {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
